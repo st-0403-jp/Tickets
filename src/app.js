6048721ecbf0de1254221cc283e39e4181edcfb0
田中　暁 <satoru_tanaka@waku-2.com>
2017-06-09 15:46:05 +0900

    【add】reactとりあえず動いた

---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const app = () => (
+    <div>
+      hello, world
+    </div>
+);
+
+export default app
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './app';
+
+render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById("root"));
